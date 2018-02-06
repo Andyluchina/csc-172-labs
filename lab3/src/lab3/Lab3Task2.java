4c48051e62bcc7a7a9db31704e2e494b36ae4370
AJ Jordan <alex@strugee.net>
2018-02-05 23:03:23 -0500

    Add WIP task 2 implementation

---
+++ lab3/src/lab3/Lab3Task2.java
@@ -0,0 +1,67 @@
+package lab3;
+
+public class Lab3Task2 {
+
+	public static void runningSum2DArray(Integer[][] arr, int dir) {
+		switch (dir) {
+		case 1:
+			// Left
+			for (Integer[] row: arr) {
+				Integer lastNum = null;
+				for (int i = 0; i < row.length; i++) {
+					if (lastNum == null) {
+						// Start of row
+						lastNum = row[i];
+					} else {
+						row[i] += lastNum;
+						lastNum = row[i];
+					}
+				}
+			}
+			break;
+		case 2:
+			// Right
+			for (Integer[] row: arr) {
+				// TODO: refactor
+				Integer lastNum = null;
+				for (int i = row.length-1; i >= 0; i--) {
+					if (lastNum == null) {
+						// Start of row
+						lastNum = row[i];
+					} else {
+						row[i] += lastNum;
+						lastNum = row[i];
+					}
+				}
+			}
+			break;
+		case 3:
+			// Up
+			// This is more brittle than the rest because it assumes 4x4.
+			// That's in the spec though so it's fine for me to not bother.
+			// Which is good, because I can't be bothered to make it flexible.
+			// TODO
+			break;
+		case 4:
+			// Down
+			// TODO
+			break;
+		default:
+			throw new Error("Invalid direction integer provided");
+		}
+		
+		Lab3Util.print2DArray(arr);
+	}
+	
+	public static void main(String[] args) {
+		Integer[][] arr = {
+				{10, 15, 30, 40},
+				{15, 5, 8, 2}, 
+				{20, 2, 4, 2},
+				{1, 4, 5, 0}
+		};
+		
+		runningSum2DArray(arr, 2);
+	}
+
+}
