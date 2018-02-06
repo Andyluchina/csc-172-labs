00f9caeeb37406681e6b932aead56dbb0ff1afba
AJ Jordan <alex@strugee.net>
2018-02-05 22:14:09 -0500

    Finish lab 3 task 2

--- lab3/src/lab3/Lab3Task2.java
+++ lab3/src/lab3/Lab3Task2.java
@@ -15,6 +15,17 @@ public class Lab3Task2 {
 			System.out.println();
 		}
 	}
+	
+	public static void print2DArray(Integer[][] grid) {
+		for (Integer[] row: grid) {
+			for (Integer num: row) {
+				// https://stackoverflow.com/a/391978/1198896
+				System.out.print(String.format("%1$-4s", num.toString()));
+				System.out.print(' ');
+			}
+			System.out.println();
+		}
+	}
 
 	public static void main(String[] args) {
 		ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(1,2000,30,400));
@@ -25,6 +36,17 @@ public class Lab3Task2 {
 		finalList.add(3, list);
 		
 		print2DList(finalList);
+		
+		Integer[][] finalArray = {
+				{10, 15, 30, 40},
+				{15, 5, 8, 2}, 
+				{20, 2, 4, 2},
+				{1, 4, 5, 0}
+		};
+		
+		System.out.println();
+
+		print2DArray(finalArray);
 	}
 
 }
