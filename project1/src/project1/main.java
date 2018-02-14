9bc8a6c983da2cfb203e30daac55f69117763eae
AJ Jordan <alex@strugee.net>
2018-02-13 20:07:54 -0500

    Implement basic board moves

--- project1/src/project1/main.java
+++ project1/src/project1/main.java
@@ -1,5 +1,6 @@
 package project1;
 
+import java.util.Scanner;
 import project1.Model;
 import project1.View;
 
@@ -8,9 +9,51 @@ public class main {
 	public static void main(String[] args) {
 		Model.init();
 		View.render(Model.getGrid());
-		System.out.println();
-		Model.moveDown();
-		View.render(Model.getGrid());
+		boolean gameInProgress = true;
+
+		/*
+		 * Apparently in order to eagerly read a character from the console,
+		 * instead of waiting for a newline, you need to put the console into
+		 * "raw" mode. Also, there apparently is no portable way to do this
+		 * built-in to Java. There goes "write once, run anywhere". See
+		 * https://stackoverflow.com/a/1066647/1198896.
+		 * 
+		 * Also, this project does not allow external dependencies. So I can't
+		 * get a library to do this, either. FML.
+		 * 
+		 * Hence, go with the shoddy implementation that requires you to press
+		 * enter. This is not the Right Way™.
+		 * 
+		 * :/
+		 * 
+		 */
+		Scanner scanner = new Scanner(System.in);
+
+		// Poor man's event loop
+		while (gameInProgress) {
+			// https://stackoverflow.com/a/13942707/1198896
+			char c = scanner.next().charAt(0);
+			switch (c) {
+			case 'w':
+				Model.moveUp();
+				break;
+			case 'a':
+				Model.moveLeft();
+				break;
+			case 's':
+				Model.moveDown();
+				break;
+			case 'd':
+				Model.moveRight();
+				break;
+			case 'q':
+				break;
+			case 'r':
+				break;
+			}
+
+			View.render(Model.getGrid());
+		}
 	}
 
 }
