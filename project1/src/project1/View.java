d249b3bfa8c9da832461eed5ffecba23f2d1d018
AJ Jordan <alex@strugee.net>
2018-02-13 20:35:48 -0500

    Add nice Unicode box printing

--- project1/src/project1/View.java
+++ project1/src/project1/View.java
@@ -6,7 +6,7 @@ public class View {
 	// % for i in $(seq 50); do printf '\\r\\n'; done
 	final static String FIFTY_NEWLINES = "\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n";
 
-	// https://stackoverflow.com/a/38365871/1198896
+	// https://stackoverflow.com/a/2979383/1198896#comment41661944_13542582
 	private static void clear() {
 		System.out.println(FIFTY_NEWLINES);
 	}
@@ -15,13 +15,22 @@ public class View {
 	public static void render(Integer[][] grid) {
 		clear();
 
+		int middleLinesPrinted = 0;
+		System.out.println("┌─────┬─────┬─────┬─────┐");
 		for (Integer[] row: grid) {
+			System.out.print("│");
 			for (Integer num: row) {
 				// https://stackoverflow.com/a/391978/1198896
 				System.out.print(String.format("%1$-4s", num.toString()));
 				System.out.print(' ');
+				System.out.print('│');
 			}
 			System.out.println();
+			if (middleLinesPrinted < 3) {
+				System.out.println("├─────┼─────┼─────┼─────┤");
+				middleLinesPrinted++;
+			}
 		}
+		System.out.println("└─────┴─────┴─────┴─────┘");
 	}
 }
