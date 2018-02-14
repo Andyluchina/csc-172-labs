462cca04539c439a9c304f310bc0bc63043ce536
AJ Jordan <alex@strugee.net>
2018-02-13 20:23:45 -0500

    Clear the screen between renders

--- project1/src/project1/View.java
+++ project1/src/project1/View.java
@@ -1,8 +1,20 @@
 package project1;
 
+import java.io.IOException;
+
 public class View {
-	// Copied from lab 3 util
+	// % for i in $(seq 50); do printf '\\r\\n'; done
+	final static String FIFTY_NEWLINES = "\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n";
+
+	// https://stackoverflow.com/a/38365871/1198896
+	private static void clear() {
+		System.out.println(FIFTY_NEWLINES);
+	}
+
+	// Copied from lab 3 util; lightly modified
 	public static void render(Integer[][] grid) {
+		clear();
+
 		for (Integer[] row: grid) {
 			for (Integer num: row) {
 				// https://stackoverflow.com/a/391978/1198896
