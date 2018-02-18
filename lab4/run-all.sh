351bc1a4f0cd2ad2f3f6bcfc1c01e88da2f7cc74
AJ Jordan <alex@strugee.net>
2018-02-18 18:28:19 -0500

    Add an XXX note

--- lab4/run-all.sh
+++ lab4/run-all.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 javac *.java
+# XXX we should renice the process
 for prog in TwoSum TwoSumFast ThreeSum ThreeSumFast; do echo ===== Using $prog.java =====; for txtfile in 1Kints 2Kints 4Kints 8Kints 16Kints 32Kints; do time java -classpath . $prog $txtfile.txt; done; done
 
