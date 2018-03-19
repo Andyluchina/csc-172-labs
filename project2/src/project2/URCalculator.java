e6a252879ac05650e392e743c53e7c312d08361f
AJ Jordan <alex@strugee.net>
2018-03-19 17:13:24 -0400

    Add debug commands to the REPL

--- project2/src/project2/URCalculator.java
+++ project2/src/project2/URCalculator.java
@@ -1,14 +1,32 @@
 package project2;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class URCalculator {
 
 	public static void main(String[] args) {
+		@SuppressWarnings("resource")
 		Scanner scanner = new Scanner(System.in);
+		boolean debug = true;
+
 		while (true) {
-			String line = scanner.next();
-			if (line == "exit") System.exit(0);
+			System.out.print("> ");
+			String line = scanner.nextLine().trim();
+			switch (line) {
+			case "exit":
+				System.exit(0);
+				break;
+			case "debug enable":
+				debug = true;
+				break;
+			case "debug disable":
+				debug = false;
+				break;
+			default:
+				ArrayList<Token> tokens = Lexer.tokenize(line);
+				if (debug) System.out.println(tokens);
+			}
 		}
 	}
 
