55a6245b521aef530eb214aed0d7459f827f4619
AJ Jordan <alex@strugee.net>
2018-04-15 20:51:05 -0400

    Implement much of project 3

--- project3/src/HuffmanSubmit/HuffmanSubmit.java
+++ project3/src/HuffmanSubmit/HuffmanSubmit.java
@@ -12,7 +12,7 @@ import HuffmanSubmit.BinaryIn;
 import HuffmanSubmit.BinaryOut;
 
 public class HuffmanSubmit implements Huffman {
-  
+
 	public class Node {
 		public Node(Byte b) {
 			this.val = b;
@@ -48,7 +48,23 @@ public class HuffmanSubmit implements Huffman {
 		Byte val;
 		boolean[] prefixCode;
 	}
- 
+
+	public Node treeFromFreqMap(HashMap<Byte, Integer> freq) {
+		ArrayList<Entry<Byte, Integer>> freqEntries = new ArrayList<Entry<Byte, Integer>>();
+		freqEntries.addAll(freq.entrySet());
+		Collections.sort(freqEntries, Entry.comparingByValue());
+		Node htree = null;
+		// XXX produce flatter trees
+		for (Entry<Byte, Integer> e: freqEntries) {
+			if (htree == null) {
+				htree = new Node(e.getKey());
+			} else {
+				htree = new Node(e.getKey(), htree);
+			}
+		}
+		return htree;
+	}
+
 	public void encode(String inputFile, String outputFile, String freqFile){
 		BinaryIn in = new BinaryIn(inputFile);
 		BinaryOut freqOut = new BinaryOut(freqFile);
@@ -62,18 +78,7 @@ public class HuffmanSubmit implements Huffman {
 			}
 		}
 
-		ArrayList<Entry<Byte, Integer>> freqEntries = new ArrayList<Entry<Byte, Integer>>();
-		freqEntries.addAll(freq.entrySet());
-		Collections.sort(freqEntries, Entry.comparingByValue());
-		Node htree = null;
-		// XXX produce flatter trees
-		for (Entry<Byte, Integer> e: freqEntries) {
-			if (htree == null) {
-				htree = new Node(e.getKey());
-			} else {
-				htree = new Node(e.getKey(), htree);
-			}
-		}
+		Node htree = treeFromFreqMap(freq);
 
 		HashMap<Byte, boolean[]> prefixCodes = new HashMap<Byte, boolean[]>();
 		htree.annotatePrefixCodes(prefixCodes);
@@ -106,22 +111,118 @@ public class HuffmanSubmit implements Huffman {
 		}
 		freqOut.flush();
 		freqOut.close();
-   }
+	}
 
 
-   public void decode(String inputFile, String outputFile, String freqFile){
-	   // TODO
-   }
+	public void decode(String inputFile, String outputFile, String freqFile){
+		// XXX prefixCode should really be some sort of number
+		HashMap<Byte, Integer> freq = new HashMap<Byte, Integer>();
+		BinaryIn freqIn = new BinaryIn(freqFile);
+		// Whether or not we're handling the bit array record or the frequency record
+		boolean handlingFreq = false;
+		String record = "";
+		String characterData = "";
+		while (!freqIn.isEmpty()) {
+			char c = freqIn.readChar();
+			if (!handlingFreq) {
+				if (c == '1' || c == '0') {
+					record += c;
+					continue;
+				} else if (c == ':') {
+					characterData = record;
+					handlingFreq = true;
+					record = "";
+					continue;
+				}
+				// XXX better error message
+				throw new RuntimeException("unexpected frequency file bit array character while decoding");
+			} else {
+				if (c == '\n') {
+					handlingFreq = false;
+					BitSet bitset = new BitSet(8);
+					for (int i = 0; i < 8; i++) {
+						if (characterData.charAt(i) == '1') {
+							bitset.set(i);
+						} else {
+							bitset.clear(i);
+						}
+					}
+
+					// We special-case this because if the bitset is empty, it returns a byte array of length 0
+					byte b;
+					if (bitset.isEmpty()) {
+						b = 0;
+					} else {
+						b = bitset.toByteArray()[0];
+					}
+
+					freq.put(b, Integer.valueOf(record));
+					record = "";
+				} else {
+					record += c;
+				}
+			}
+		}
+
+		Node htree = treeFromFreqMap(freq);
+
+		HashMap<Byte, boolean[]> _prefixCodes = new HashMap<Byte, boolean[]>();
+		htree.annotatePrefixCodes(_prefixCodes);
+		// Switch keys and values
+		// XXX just do it right the first time
+		HashMap<Boolean[], Byte> prefixCodes = new HashMap<Boolean[], Byte>();
+		for (Entry<Byte, boolean[]> e: _prefixCodes.entrySet()) {
+			// This is ridiculous
+			Boolean[] l = new Boolean[e.getValue().length];
+			int i = 0;
+			for (boolean b: e.getValue()) {
+				l[i] = Boolean.valueOf(b);
+				i++;
+			}
+			prefixCodes.put(l, e.getKey());
+		}
+
+		BinaryIn in = new BinaryIn(inputFile);
+		BinaryOut out = new BinaryOut(outputFile);
+		ArrayList<Boolean> bits = new ArrayList<Boolean>();
+		while (!in.isEmpty()) {
+			boolean bit = in.readBoolean();
+			bits.add(bit);
+			boolean foundKey = false;
+			Boolean[] keyWeFound = null;
+			// Can't use HashMap#contains because it uses object id, not value
+			for (Boolean[] key: prefixCodes.keySet()) {
+				// Java didn't like a Boolean[] cast so here we fuckin go
+				Boolean[] cmp = new Boolean[bits.size()];
+				for (int i = 0; i < cmp.length; i++) {
+					cmp[i] = bits.get(i);
+				}
+				if (Arrays.equals(key, cmp)) {
+					foundKey = true;
+					// The entire premise of this variable is ridiculous
+					keyWeFound = key;
+					break;
+				}
+			}
+			if (foundKey) {
+				out.write(prefixCodes.get(keyWeFound));
+				// XXX remove
+				out.flush();
+			}
+		}
+		out.flush();
+		out.close();
+	}
 
 
 
 
-   public static void main(String[] args) {
-      Huffman  huffman = new HuffmanSubmit();
-		huffman.encode("ur.jpg", "ur.enc", "freq.txt");
-		huffman.decode("ur.enc", "ur_dec.jpg", "freq.txt");
+	public static void main(String[] args) {
+		Huffman  huffman = new HuffmanSubmit();
+		huffman.encode("alice30.txt", "alice30.enc", "freq.txt");
+		huffman.decode("alice30.enc", "alice30_dec.txt", "freq.txt");
 		// After decoding, both ur.jpg and ur_dec.jpg should be the same. 
 		// On linux and mac, you can use `diff' command to check if they are the same. 
-   }
+	}
 
 }
---
+++ project3/src/README.md
@@ -0,0 +1,26 @@
+# Huffman tree compression implementation
+
+<!-- You can see this README rendered _much_ nicer online: https://github.com/strugee/csc-172-labs/blob/master/project3/README.md -->
+
+This project contains an implementation of project 3, a Huffman encoding compression implementation.
+
+## Running
+
+Run `HuffmanSubmit.java`.
+
+## Bugs
+
+* `freq.txt` doesn't use CRLF on Windows
+* It's super slow
+
+## Git
+
+The Git repository for this project, complete with revision history, can be cloned from [GitHub](https://github.com/strugee/csc-172-labs).
+
+## Author
+
+AJ Jordan <alex@strugee.net>, <ajord17@u.rochester.edu>
+
+## License
+
+Creative Commons Zero 1.0
